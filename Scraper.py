78fcdf29ddc358b6f9cce88704653a579295f0b8
John Mitchell <einbein@Johns-MacBook-Pro.local>
2022-12-17 20:40:49 -0500

    Prototype ocr price read from grid

--- Scraper.py
+++ Scraper.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import numpy as np
+import cv2
+import pytesseract
 from time import sleep
 #from selenium.webdriver.support.ui import WebDriverWait
 #from selenium.common.exceptions import TimeoutException
@@ -13,7 +16,9 @@ def get_driver():
 
     chrome_options = Options()
     #chrome_options.add_argument('--headless')
-    return webdriver.Chrome(options = chrome_options)
+    driver = webdriver.Chrome(options = chrome_options)
+    driver.implicitly_wait(20)
+    return driver
 
 def get_flights(driver, source, target):
     '''
@@ -29,7 +34,6 @@ def get_flights(driver, source, target):
     dep = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input")
     #dep = driver.find_element(By.XPATH, "//*[@id='ow49']/div[1]/div/div/input")
     dep.click()
-    sleep(1)
     #dep_dd = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[2]/div[2]/div[1]/div/input")
     dep_dd = driver.find_element(By.XPATH, "//*[@id='i15']/div[6]/div[2]/div[2]/div[1]/div/input")
     dep_dd.send_keys(source)
@@ -38,7 +42,6 @@ def get_flights(driver, source, target):
     # Enter the destination
     dest = driver.find_element(By.XPATH, "//input[@class='II2One j0Ppje zmMKJ LbIaRd' and @placeholder='Where to?']")
     dest.click()
-    sleep(1)
     dest_dd = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[2]/div[2]/div[1]/div/input")
     dest_dd.send_keys(target)
     dest_dd.send_keys(Keys.ENTER)
@@ -46,7 +49,44 @@ def get_flights(driver, source, target):
     # Search
     search = driver.find_element(By.XPATH, "//span[text()='Search']")
     search.click()
+
+    # Select the date grid
+    dg = driver.find_element(By.XPATH, "//span[text()='Date grid']")
+    dg.click()
     sleep(5)
+
+    # This opens a canvas with a grid of prices
+    canvas = driver.find_element(By.XPATH, "//canvas")
+    
+    # Take a screenshot of the canvas element
+    screenshot = canvas.screenshot_as_png
+    with open('graph.png', 'wb') as f:
+        f.write(screenshot)
+
+    extract_prices('graph.png')
+
+def extract_prices(ss):
+    '''
+    Extract airfare information from a screenshot of a price grid
+    '''
+
+    # Load the screenshot image
+    image = cv2.imread(ss)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Otsu's thresholding to binarize the image
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    # Set the custom configuration for pytesseract
+    custom_config = r'--psm 6 --oem 3'  
+
+    # Use Tesseract to extract text from the image
+    text = pytesseract.image_to_string(thresh, config=custom_config)
+
+    print(text)
+
 
 if __name__ == '__main__':
     driver = get_driver()
